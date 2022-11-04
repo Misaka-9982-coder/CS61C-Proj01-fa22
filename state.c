93b30be15bf00b90b13bf75f6bbe5ebbf4c33fec
misaka <1784321894@qq.com>
2022-11-04 11:59:22 +0800

    CS61C Proj01: Task 7 main fix strcpy

--- state.c
+++ state.c
@@ -382,7 +382,7 @@ game_state_t* load_board(char* filename) {
     buff = malloc(1024 * 1024 * sizeof(char));
     fgets(buff, 1024 * 1024, fp);
     game_state->board[i] = malloc((strlen(buff)) * sizeof(char));
-    strcpy(game_state->board[i], buff);
+    strncpy(game_state->board[i], buff, strlen(buff));
     game_state->board[i][strlen(buff) - 1] = '\0';
     free(buff);
   }
